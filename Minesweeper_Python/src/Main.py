bab37fa04ed10136e8bea632e597d1b2355bdd68
s4ndhyac <s4ndhyac@gmail.com>
2018-12-08 14:46:18 -0800

    tank solver

--- Minesweeper_Python/src/Main.py
+++ Minesweeper_Python/src/Main.py
@@ -18,7 +18,7 @@
 #						-m Use ManualAI instead of MyAI.
 #						-r Use RandomAI instead of MyAI.
 #						-f [InputPath] [OutputFile]
-#						   First is absolute path to Minesweeper World file or 
+#						   First is absolute path to Minesweeper World file or
 #						   directory containing Minesweeper World files.
 #						   Second is the file name of the .txt file you wish
 #						   to write your results to.
@@ -30,7 +30,7 @@
 #						-h Displays help menu and quits.
 #
 #				- The default AI type is MyAI.
-#				
+#
 #				- When using -f, the [OutputFile] should only be used when
 #				  [InputPath] is a folder of world files. If not, then
 #				  [OutputPath] is useless.
@@ -52,113 +52,122 @@ from MyAI import MyAI
 
 def main():
 
-	# Create parser
-	parser = argparse.ArgumentParser(description="", prog="Main.py", usage="%(prog)s [options]", epilog="Note: [options] can be in any order")
+    # Create parser
+    parser = argparse.ArgumentParser(
+        description="", prog="Main.py", usage="%(prog)s [options]", epilog="Note: [options] can be in any order")
 
-	# Parse options																				# Help is default
-	parser.add_argument("-f", "-F", help="file or directory name", nargs='*')								# File path
-	parser.add_argument("-m", "-M", help="enable ManualAI mode", action="store_true")			# ManualAI
-	parser.add_argument("-r", "-R", help="enable RandomAI mode", action="store_true")			# RandomAI
-	parser.add_argument("-v", "-V", help="enable verbose mode", action="store_true")			# Verbose
-	parser.add_argument("-d", "-D", help="enable debug mode", action="store_true")				# Debug
+    # Parse options																				# Help is default
+    parser.add_argument(
+        "-f", "-F", help="file or directory name", nargs='*')								# File path
+    parser.add_argument("-m", "-M", help="enable ManualAI mode",
+                        action="store_true")			# ManualAI
+    parser.add_argument("-r", "-R", help="enable RandomAI mode",
+                        action="store_true")			# RandomAI
+    parser.add_argument("-v", "-V", help="enable verbose mode",
+                        action="store_true")			# Verbose
+    parser.add_argument("-d", "-D", help="enable debug mode",
+                        action="store_true")				# Debug
 
-	args = parser.parse_args()
-	
-	inputFile = None
-	outputFile = None
-	filepath = args.f
-	if filepath:
-		if len(filepath) == 2:
-			inputFile = filepath[0]
-			outputFile = filepath[1]
-		elif len(filepath) == 1:
-			inputFile = filepath[0]
-		else:
-			print("ERROR: -f takes 1 or 2 arguments only!")
-			return
-	verbose = args.v
-	debug = args.d
+    args = parser.parse_args()
 
-	if args.m:
-		aiType = "manual"
-	elif args.r:
-		aiType = "random"
-	elif not args.m and not args.r:
-		aiType = "myai"
+    inputFile = None
+    outputFile = None
+    filepath = args.f
+    if filepath:
+        if len(filepath) == 2:
+            inputFile = filepath[0]
+            outputFile = filepath[1]
+        elif len(filepath) == 1:
+            inputFile = filepath[0]
+        else:
+            print("ERROR: -f takes 1 or 2 arguments only!")
+            return
+    verbose = args.v
+    debug = args.d
 
-	if inputFile:
-		# If inputFile is a directory
-		if (os.path.isdir(inputFile)):
-			listOfWorlds = None
+    if args.m:
+        aiType = "manual"
+    elif args.r:
+        aiType = "random"
+    elif not args.m and not args.r:
+        aiType = "myai"
 
-			try:
-				directory = os.walk(inputFile)
-			except:
-				print("ERROR: Failed to open directory")
-				return
+    if inputFile:
+        # If inputFile is a directory
+        if (os.path.isdir(inputFile)):
+            listOfWorlds = None
 
-			numScores = 0
-			sumScores = 0
+            try:
+                directory = os.walk(inputFile)
+            except:
+                print("ERROR: Failed to open directory")
+                return
 
-			scoreBeg = 0
-			scoreInt = 0
-			scoreExp = 0
-			for dirpath, _, filenames in directory:
-				for filename in filenames:
-					f = os.path.join(dirpath, filename)
+            numScores = 0
+            sumScores = 0
 
-					world = World(filename=f, aiType=aiType, verbose=verbose, debug=debug)
+            scoreBeg = 0
+            scoreInt = 0
+            scoreExp = 0
+            for dirpath, _, filenames in directory:
+                for filename in filenames:
+                    f = os.path.join(dirpath, filename)
 
-					score = world.run()
-					if score == 1:
-						scoreBeg += 1
-					elif score == 2:
-						scoreInt += 1
-					elif score == 3:
-						scoreExp += 1
+                    world = World(filename=f, aiType=aiType,
+                                  verbose=verbose, debug=debug)
 
-					numScores += 1
-					sumScores += score
-					
-			print("---------------Your agent's results:---------------")
-			print("Beginner: {} \tIntermediate: {} \tExpert: {}".format(scoreBeg, scoreInt, scoreExp))
-			print("Cumulative Score: " + str(sumScores))
+                    score = world.run()
+                    if score == 1:
+                        scoreBeg += 1
+                    elif score == 2:
+                        scoreInt += 1
+                    elif score == 3:
+                        scoreExp += 1
 
-			if outputFile:
-				currDirectory = os.path.dirname(__file__)
-				outputFilePath = os.path.join(currDirectory, outputFile)
-				print(outputFilePath)
-				try:
-					with open(outputFilePath, 'w') as file:
-						file.write("easy: " + str(scoreBeg) + "\n")
-						file.write("medium: " + str(scoreInt) + "\n")
-						file.write("expert: " + str(scoreExp) + "\n")
-						file.write("score: " + str(sumScores))
-				except:
-					print("ERROR: Could not open file for writing!")
+                    numScores += 1
+                    sumScores += score
 
-		# If inputFile is a world file
-		elif (os.path.isfile(inputFile)):
-			world = World(filename=inputFile, aiType=aiType, verbose=verbose, debug=debug)
-			score = world.run()
-			if score > 0:
-			    print("WORLD COMPLETE")
-			else:
-			    print("WORLD INCOMPLETE")
+            print("---------------Your agent's results:---------------")
+            print("Beginner: {} \tIntermediate: {} \tExpert: {}".format(
+                scoreBeg, scoreInt, scoreExp))
+            print("Cumulative Score: " + str(sumScores))
 
-		# If inputFileis an invalid path
-		else:
-			print("ERROR: Directory or file does not exist!")
+            if outputFile:
+                currDirectory = os.path.dirname(__file__)
+                outputFilePath = os.path.join(currDirectory, outputFile)
+                print(outputFilePath)
+                try:
+                    with open(outputFilePath, 'w') as file:
+                        file.write("easy: " + str(scoreBeg) + "\n")
+                        file.write("medium: " + str(scoreInt) + "\n")
+                        file.write("expert: " + str(scoreExp) + "\n")
+                        file.write("score: " + str(sumScores))
+                except:
+                    print("ERROR: Could not open file for writing!")
 
-	else:
-		world = World(aiType=aiType, verbose=verbose, debug=debug)
-		score = world.run()
-		print("Your AI scored: " + str(score))
-		if score == 0:
-			print("WORLD INCOMPLETE")
-		else:
-			print("WORLD COMPLETE")
-		
+        # If inputFile is a world file
+        elif (os.path.isfile(inputFile)):
+            world = World(filename=inputFile, aiType=aiType,
+                          verbose=verbose, debug=debug)
+            score = world.run()
+            if score > 0:
+                print("WORLD COMPLETE")
+            else:
+                print("WORLD INCOMPLETE")
+
+        # If inputFileis an invalid path
+        else:
+            print("ERROR: Directory or file does not exist!")
+
+    else:
+        world = World(aiType=aiType, verbose=verbose, debug=debug)
+        score = world.run()
+        print("Your AI scored: " + str(score))
+        if score == 0:
+            print("WORLD INCOMPLETE")
+        else:
+            print("WORLD COMPLETE")
+
 
 if __name__ == "__main__":
-	main()
+    main()
--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -15,6 +15,9 @@
 from AI import AI
 from Action import Action
 from enum import Enum
+import queue
+import copy
+
 
 class Cell:
     def __init__(self, cell_state, percept, mine_probability, xPos, yPos):
@@ -25,6 +28,8 @@ class Cell:
         self.yPos = yPos
         self.isSafe = False
         self.isMine = False
+        self.adjCells = []
+
 
 class CellState(Enum):
     COVERED = 0
@@ -32,11 +37,13 @@ class CellState(Enum):
     FLAGGED = 2
     UNFLAGGED = 3
 
+
 class MyAI(AI):
 
     def __init__(self, rowDimension, colDimension, totalMines, startY, startX):
         self.rowDimension = rowDimension
         self.colDimension = colDimension
+        self.totalMines = totalMines
         self.cellsRemaining = rowDimension * colDimension
         self.minesRemaining = totalMines
         initialMineProbability = totalMines/(rowDimension * colDimension)
@@ -46,17 +53,37 @@ class MyAI(AI):
             for j in range(colDimension):
                 cell = Cell(CellState.COVERED, None,
                             initialMineProbability, i, j)
+                xPosBeg = i - 1 if i - 1 >= 0 else i
+                xPosEnd = i + 1 if i + 1 <= rowDimension - 1 else i
+                yPosBeg = j - 1 if j - 1 >= 0 else j
+                yPosEnd = j + 1 if j + 1 <= colDimension - 1 else j
+                for x in range(xPosBeg, xPosEnd + 1):
+                    for y in range(yPosBeg, yPosEnd + 1):
+                        cell.adjCells.append((x, y))
                 row.append(cell)
             self.cells.append(row)
         self.exploredCells = []
         self.safeCells = []
         self.lastX = startX
         self.lastY = startY
+        self.cellsCopy = []
+        self.backtrackingSolution = []
+        self.backtrackOverAllOrOnlyBoundary = False
 
-    def get_adj_cells(self, xPos, yPos, rowDimension, colDimension):
+    def isAdjCellUncovered(self, xPos, yPos, rowDimension, colDimension):
+        xPosBeg = xPos - 1 if xPos - 1 >= 0 else xPos
+        xPosEnd = xPos + 1 if xPos + 1 <= rowDimension - 1 else xPos
+        yPosBeg = yPos - 1 if yPos - 1 >= 0 else yPos
+        yPosEnd = yPos + 1 if yPos + 1 <= colDimension - 1 else yPos
+        for i in range(xPosBeg, xPosEnd + 1):
+            for j in range(yPosBeg, yPosEnd + 1):
+                if self.cells[i][j].cell_state == CellState.UNCOVERED:
+                    return True
+        return False
+
+    def get_adj_cells(self, cells, xPos, yPos, rowDimension, colDimension):
         ''' returns the list of adjacent covered and flagged cells
             as a list of Cell class objects'''
-
         adj_c_cells = []
         adj_f_cells = []
         xPosBeg = xPos - 1 if xPos - 1 >= 0 else xPos
@@ -65,13 +92,13 @@ class MyAI(AI):
         yPosEnd = yPos + 1 if yPos + 1 <= colDimension - 1 else yPos
         for i in range(xPosBeg, xPosEnd + 1):
             for j in range(yPosBeg, yPosEnd + 1):
-                if self.cells[i][j].cell_state == CellState.COVERED:
-                    adj_c_cells.append((i,j))
-                if self.cells[i][j].cell_state == CellState.FLAGGED:
-                    adj_f_cells.append((i,j))
+                if cells[i][j].cell_state == CellState.COVERED:
+                    adj_c_cells.append((i, j))
+                if cells[i][j].cell_state == CellState.FLAGGED:
+                    adj_f_cells.append((i, j))
         return (adj_c_cells, adj_f_cells)
 
-    def decideAction(self):
+    def decideActionByProbability(self):
         minProb = 1.1
         for i in range(self.rowDimension):
             for j in range(self.colDimension):
@@ -89,52 +116,139 @@ class MyAI(AI):
         if cells > 0:
             return mines/cells
 
-    def getAction(self, number: int) -> "Action Object":
+    def getBoundaryCells(self):
+        '''Gets boundary Cells and all Covered cells'''
+        boundaryList = []
+        coveredList = []
+        for cellRow in self.cells:
+            for cell in cellRow:
+                if self.cells[cell.xPos][cell.yPos].cell_state != CellState.COVERED:
+                    continue
+                coveredList.append((cell.xPos, cell.yPos))
+                if self.isAdjCellUncovered(cell.xPos, cell.yPos, self.rowDimension, self.colDimension):
+                    boundaryList.append((cell.xPos, cell.yPos))
+        return boundaryList, coveredList
 
+    def getIsolatedBoundarys(self, boundaryList):
+        '''Gets isolated boundary areas'''
+        # sorted(boundaryList, key=lambda element: (element[0], element[1]))
+        regions = []
+        visited = []
+        while True:
+            q = queue.Queue()
+            region = []
+            for b in boundaryList:
+                if b not in visited:
+                    q.put(b)
+                    break
+            if q.empty():
+                break
+            while not q.empty():
+                bl = q.get()
+                region.append(bl)
+                visited.append(bl)
+                for c in boundaryList:
+                    isConnected = False
+                    if c not in region:
+                        if c in self.cells[bl[0]][bl[1]].adjCells:
+                            isConnected = True
+                            break
+                if isConnected and c not in q.queue:
+                    q.put(c)
+            regions.append(region)
+        return regions
+
+    # def getSmallestIsolatedBoundary(self, regions):
+    #     '''Get smallest isolated boundary region'''
+    #     minLen = 481
+    #     minReg = []
+    #     for region in regions:
+    #         l = len(region)
+    #         if l < minLen:
+    #             minLen = l
+    #             minReg = region
+    #     return minReg
+
+    def backtrackingAlgorithm(self, borderCells, position):
+        '''Take the smallest possible area that can be considered in isolation
+          Make a guess and keep moving forward until you reach a contradiction
+          If you reach a contradiction then eliminate that assumption and try another
+          till you reach a case that solves without contradiction'''
+        flagCount = 0
+        for cellRow in self.cellsCopy:
+            for cell in cellRow:
+                if cell.isMine == True:
+                    flagCount = flagCount + 1
+                if cell.cell_state == CellState.UNCOVERED:
+                    adjCoveredCells, adjFlaggedCells = self.get_adj_cells(self.cellsCopy,
+                                                                          cell.xPos, cell.yPos, self.rowDimension, self.colDimension)
+                    if len(adjFlaggedCells) > cell.percept:
+                        return
+                    if len(cell.adjCells) - cell.percept < len(adjCoveredCells):
+                        return
+        if flagCount > self.totalMines:
+            return
+        if position == len(borderCells):
+            if not self.backtrackOverAllOrOnlyBoundary and (flagCount < self.totalMines):
+                return
+            for b in borderCells:
+                if self.cellsCopy[b[0]][b[1]].cell_state == CellState.COVERED and (self.cellsCopy[b[0]][b[1]].isMine or self.cellsCopy[b[0]][b[1]].isSafe):
+                    self.backtrackingSolution.append(b)
+            return
+        bl = borderCells[position]
+        self.cellsCopy[bl[0]][bl[1]].isMine = True
+        self.backtrackingAlgorithm(borderCells, position+1)
+        self.cellsCopy[bl[0]][bl[1]].isMine = False
+        self.cellsCopy[bl[0]][bl[1]].isSafe = True
+        self.backtrackingAlgorithm(borderCells, position + 1)
+        self.cellsCopy[bl[0]][bl[1]].isSafe = False
+
+    def getAction(self, number: int) -> "Action Object":
         self.cells[self.lastX][self.lastY].percept = number
         if number == -1:
             self.cells[self.lastX][self.lastY].cell_state = CellState.FLAGGED
         else:
             self.cells[self.lastX][self.lastY].cell_state = CellState.UNCOVERED
 
-        if (self.lastX,self.lastY) not in self.exploredCells:
-            self.exploredCells.append((self.lastX,self.lastY))
+        if (self.lastX, self.lastY) not in self.exploredCells:
+            self.exploredCells.append((self.lastX, self.lastY))
 
-        adjCells, adjFlaggedCells = self.get_adj_cells(
-            self.lastX, self.lastY, self.rowDimension, self.colDimension)
+        adjCells, adjFlaggedCells = self.get_adj_cells(self.cells,
+                                                       self.lastX, self.lastY, self.rowDimension, self.colDimension)
         adjCellsNum = len(adjCells)
         adjFlaggedCellsNum = len(adjFlaggedCells)
-        cellMineProb = 1 if number == -1 else self.getMineProbability(number, adjCellsNum)
+        cellMineProb = 1 if number == - \
+            1 else self.getMineProbability(number, adjCellsNum)
 
-        for xPos,yPos in adjCells:
+        for xPos, yPos in adjCells:
             self.cells[xPos][yPos].mine_probability = cellMineProb
-            if (xPos,yPos) not in self.exploredCells:
-                self.exploredCells.append((xPos,yPos))
+            if (xPos, yPos) not in self.exploredCells:
+                self.exploredCells.append((xPos, yPos))
             if number == 0 or number == adjFlaggedCellsNum:
-                if (xPos,yPos) not in self.safeCells:
+                if (xPos, yPos) not in self.safeCells:
                     self.cells[xPos][yPos].isSafe = True
-                    self.safeCells.append((xPos,yPos))
+                    self.safeCells.append((xPos, yPos))
             elif number - adjFlaggedCellsNum == adjCellsNum:
                 if not self.cells[xPos][yPos].isMine:
                     self.cells[xPos][yPos].isMine = True
                     self.minesRemaining = self.minesRemaining - 1
 
         if self.safeCells and len(self.safeCells):
-            for (xPos,yPos) in self.safeCells:
+            for (xPos, yPos) in self.safeCells:
                 if self.cells[xPos][yPos].cell_state == CellState.COVERED:
                     self.lastX = xPos
                     self.lastY = yPos
                     return Action(AI.Action.UNCOVER, yPos, xPos)
-                
+
         for cellRow in self.cells:
             for cell in cellRow:
                 if cell.cell_state == CellState.UNCOVERED and cell.percept > 0:
-                    cellAdjCoveredCells, cellAdjFlagged = self.get_adj_cells(
-                        cell.xPos, cell.yPos, self.rowDimension, self.colDimension)
+                    cellAdjCoveredCells, cellAdjFlagged = self.get_adj_cells(self.cells,
+                                                                             cell.xPos, cell.yPos, self.rowDimension, self.colDimension)
                     cellAdjFlaggedNum = len(cellAdjFlagged)
                     cellAdjCoveredCellsNum = len(cellAdjCoveredCells)
                     if cell.percept - cellAdjFlaggedNum == cellAdjCoveredCellsNum:
-                        for xPos,yPos in cellAdjCoveredCells:
+                        for xPos, yPos in cellAdjCoveredCells:
                             if not self.cells[xPos][yPos].isMine:
                                 self.cells[xPos][yPos].isMine = True
                                 self.minesRemaining = self.minesRemaining - 1
@@ -144,25 +258,49 @@ class MyAI(AI):
                     self.lastX = cell.xPos
                     self.lastY = cell.yPos
                     return Action(AI.Action.FLAG, cell.yPos, cell.xPos)
+
         for cellRow in self.cells:
             for cell in cellRow:
                 if cell.cell_state == CellState.UNCOVERED and cell.percept > 0:
-                    cellAdjCoveredCells, cellAdjFlagged = self.get_adj_cells(
-                        cell.xPos, cell.yPos, self.rowDimension, self.colDimension)
+                    cellAdjCoveredCells, cellAdjFlagged = self.get_adj_cells(self.cells,
+                                                                             cell.xPos, cell.yPos, self.rowDimension, self.colDimension)
                     cellAdjFlaggedNum = len(cellAdjFlagged)
                     cellAdjCoveredCellsNum = len(cellAdjCoveredCells)
                     if cell.percept == cellAdjFlaggedNum:
-                        for xPos,yPos in cellAdjCoveredCells:
-                            if (xPos,yPos) not in self.safeCells:
+                        for xPos, yPos in cellAdjCoveredCells:
+                            if (xPos, yPos) not in self.safeCells:
                                 self.cells[xPos][yPos].isSafe = True
                                 self.safeCells.append(
-                                    (xPos,yPos))
+                                    (xPos, yPos))
         if self.safeCells and len(self.safeCells) > 0:
-            for xPos,yPos in self.safeCells:
+            for xPos, yPos in self.safeCells:
                 if self.cells[xPos][yPos].cell_state == CellState.COVERED:
                     self.lastX = xPos
                     self.lastY = yPos
                     return Action(AI.Action.UNCOVER, yPos, xPos)
+
+        boundaryList, coveredList = self.getBoundaryCells()
+        self.backtrackOverAllOrOnlyBoundary = False
+        if len(coveredList) - len(boundaryList) > 8:
+            self.backtrackOverAllOrOnlyBoundary = True
+        else:
+            boundaryList = coveredList
+        if len(boundaryList) > 0:
+            isolatedBoundarys = self.getIsolatedBoundarys(boundaryList)
+            for smallestIsolatedBoundary in isolatedBoundarys:
+                self.cellsCopy = copy.deepcopy(self.cells)
+                self.backtrackingSolution = []
+                self.backtrackingAlgorithm(smallestIsolatedBoundary, 0)
+                if self.backtrackingSolution and len(self.backtrackingSolution) > 0:
+                    for b in self.backtrackingSolution:
+                        if self.cellsCopy[b[0]][b[1]].cell_state == CellState.COVERED:
+                            if self.cellsCopy[b[0]][b[1]].isMine:
+                                self.cells[b[0]][b[1]].isMine = True
+                                return Action(AI.Action.FLAG, b[1], b[0])
+                            elif self.cellsCopy[b[0]][b[1]].isSafe:
+                                self.cells[b[0]][b[1]].isSafe = True
+                                self.safeCells.append((b[0], b[1]))
+                                return Action(AI.Action.UNCOVER, b[1], b[0])
 
         if self.cellsRemaining > len(self.exploredCells) and self.minesRemaining > number:
             currMinePercept = 0 if number == -1 else number
@@ -170,7 +308,7 @@ class MyAI(AI):
                 self.minesRemaining - currMinePercept, self.cellsRemaining - len(self.exploredCells))
             for xPos in range(self.rowDimension):
                 for yPos in range(self.colDimension):
-                    if (xPos,yPos) not in self.exploredCells and (xPos,yPos) not in adjCells and (xPos,yPos) not in adjFlaggedCells:
+                    if (xPos, yPos) not in self.exploredCells and (xPos, yPos) not in adjCells and (xPos, yPos) not in adjFlaggedCells:
                         cell.mine_probability = mineProbRemaining
                         self.cells[xPos][yPos].mine_probability = mineProbRemaining
-        return self.decideAction()
+        return self.decideActionByProbability()
